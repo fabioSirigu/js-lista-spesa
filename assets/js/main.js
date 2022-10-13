89cf0e8a1ab0f120eccae9a35c2fe21a3716369e
naz1ne <fabio.sirigu.1995@gmail.com>
2022-10-13 14:47:02 +0200

    add ex lista della spesa whit while loop

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// creo un array con la lista della spesa
+
+const discountList = [
+      'comprare un fusto di birra',
+      'comprare i bicchieri per la birra',
+      'comprare i sottobicchieri per la i bicchieri per la birra',
+      'comprare un tavolo dove mettere il fusto di birra'
+];
+
+// creo un ciclo per visualizzare singolarmente ogni punto della lista della spesa con while
+let i = 0
+while (i < discountList.length) {
+      let product = discountList[i]
+      console.log(product)
+
+      const ulElement = document.querySelector('.card > ul')
+      ulElement.innerHTML += `<li class="list-group-item">${(product)}</li>`
+      
+      i++
+}
